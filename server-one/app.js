32a8e8e510941799fbb2682096089e59dc2e803f
Sami Bäckman <samiab@metropolia.fi>
2022-10-31 10:06:43 +0200

    express tehtävät

---
+++ server-one/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('index')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+app.use(express.static('public'));
+
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    name: 'Frank',
+    birthdate: '2010-12-25',
+    weight: 5,
+  };
+  res.json(cat);
+});
